b37b9eb0087065a428cf0b5352652307f859984c
Luke M <lukem@Lukess-Work-Laptop.local>
2024-10-31 18:22:21 +0200

    Audio bible editor

---
+++ app/components/MediaPlayer.tsx
@@ -0,0 +1,45 @@
+"use client"; 
+import React from 'react'
+import { Badge } from '../components/ui/Badge'
+import { IconUpload, IconSettings, IconExclamationCircle } from '@tabler/icons-react';
+import Button from '../components/Button';
+import { cn } from '@utils/clsx';
+
+interface MediaPlayerProps {
+  type: 'image' | 'video';
+  source: string;
+}
+
+const MediaPlayer: React.FC<MediaPlayerProps> = ({ type, source }) => {
+  return (
+    <div className="media-player">
+            <div className={cn(type === 'image' ? "border-b mb-2":"border-t  my-2  border-b","flex items-center py-2.5 px-2 dark:border-zinc-800 border-zinc-200 justify-between")}>
+          <Badge variant="destructive">NTV</Badge>
+          <div className="flex items-center gap-[5px]">
+          <Button label="Mark" />
+      <Button label="1" />
+       <Button icon={<IconSettings
+        size={14} 
+        stroke={2}  
+        strokeLinejoin="miter"
+      />}/>
+            </div>      </div>
+      {type === 'image' ? (
+      
+        <img src={source} alt="Media content" className="w-full h-[25vh]" />
+      ) : (
+        <iframe
+          width="100%"
+          height="315"
+          src={`https://www.youtube.com/embed/${source}`}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        />
+      )}
+    </div>
+  );
+};
+
+export default MediaPlayer; 
